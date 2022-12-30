12280c025beb304b2b7e93daac7a6d01e5083453
ElizavetaNoskova <wliza68@gmail.com>
2022-12-30 09:22:59 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        /*task2();
+        task3();*/
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
